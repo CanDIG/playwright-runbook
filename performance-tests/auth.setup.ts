c52e5017193626248691b11e19ec47cd4a0faeac
Son Chau <soncqb@gmail.com>
2024-10-02 13:35:30 -0700

    Create auth.setup.ts

---
+++ performance-tests/auth.setup.ts
@@ -0,0 +1,55 @@
+import { test as setup, expect } from "@playwright/test";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+const authFile = path.join(__dirname, "../.auth/user.json");
+
+setup("authenticate", async ({ page }) => {
+  const response = await fetch(
+    `http://candig.docker.internal:8080/auth/realms/candig/protocol/openid-connect/token`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        username: process.env.CANDIG_USERNAME!,
+        password: process.env.CANDIG_PASSWORD!,
+        client_id: "local_candig",
+        client_secret: process.env.CLIENT_SECRET!,
+        grant_type: "password",
+        scope: "openid",
+      }),
+    }
+  );
+
+  if (response.ok) {
+    const { access_token, refresh_token } = await response.json();
+
+    await page.context().addCookies([
+      {
+        name: "refresh_token",
+        value: refresh_token,
+        domain: "localhost",
+        path: "/",
+        httpOnly: true,
+        secure: false,
+        sameSite: "Lax",
+      },
+      {
+        name: "access_token",
+        value: access_token,
+        domain: "localhost",
+        path: "/",
+        httpOnly: true,
+        secure: false,
+        sameSite: "Lax",
+      },
+    ]);
+    await page.context().storageState({ path: authFile });
+  } else {
+    throw new Error(`Failed to authenticate: ${response.status}`);
+  }
+});
